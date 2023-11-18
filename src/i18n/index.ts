2c049328fcbbfe9c0197ce469aa892e0c23bdc2a
Baptiste Marcon <baptistemcn@icloud.com>
2023-11-18 15:57:09 +0100

    feat(i18n): add translation system

---
+++ src/i18n/index.ts
@@ -0,0 +1,1 @@
+export { ReProvider } from "./Provider";
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from "expo-status-bar";
+import { useTranslation } from "react-i18next";
 import { StyleSheet, Text, View } from "react-native";
 
+import { ReProvider } from "./i18n";
+
 export const Entry = () => {
+  const { t } = useTranslation();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ReProvider>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <Text>{t("name")}</Text>
+        <StatusBar style="auto" />
+      </ReProvider>
     </View>
   );
 };
