ce4e179b5e8b592ace3d2731a00841dbb82b2803
Baptiste Marcon <baptistemcn@icloud.com>
2023-12-08 00:01:07 +0100

    feat(onboarding): style and flow

--- src/authentication/Onboarding/Onboarding.tsx
+++ src/authentication/Onboarding/Onboarding.tsx
@@ -1,7 +1,10 @@
-import { useTheme } from "@rneui/themed";
-import { Box, ReButton, ReText } from "@ui";
 import { useTranslation } from "react-i18next";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+import { useTheme } from "@rneui/themed";
+
+import { Box, ReButton, ReText } from "@ui";
+import { LanguagePicker } from "../components";
 
 export const Onboarding = () => {
   const { theme } = useTheme();
@@ -13,17 +16,21 @@ export const Onboarding = () => {
       flex={1}
       flexDirection={"column"}
       paddingTop={insets.top}
+      paddingBotton={insets.bottom}
       width={"100%"}
     >
-      <Box flex={1} flexDirection={"column"} maxHeight={"33%"}>
+      <Box flex={1} flexDirection={"column"} maxHeight={"15%"}>
         <ReText fontFamily="Bold" fontSize={32} letterSpacing={3}>
           {t("name").toUpperCase()}
         </ReText>
       </Box>
-      <Box flex={1} flexDirection={"column"} maxHeight={"20%"}>
+      <Box flex={1} flexDirection={"column"} maxHeight={"30%"}>
         <ReText>{t("onboarding.message.welcome")}</ReText>
       </Box>
-      <Box flex={1} flexDirection={"column"} maxHeight={"33%"}>
+      <Box flex={1} flexDirection={"column"} maxHeight={"15%"}>
+        <LanguagePicker />
+      </Box>
+      <Box flex={1} flexDirection={"column"}>
         <Box margin={theme.spacing.md}>
           <ReText color={theme.colors.grey2} fontFamily="Light">
             {t("onboarding.message.start")}
