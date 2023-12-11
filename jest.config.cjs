102c8100364120eb97dc1fc145e4e004e55260ad
Baptiste Marcon <baptistemcn@icloud.com>
2023-12-11 17:05:59 +0100

    chore(deps): add uuid

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   transformIgnorePatterns: [
       "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@rneui)",
   ],
+  moduleNameMapper: {
+    "uuid": require.resolve('uuid'),
+  },
   coveragePathIgnorePatterns: ["index.ts", "App.tsx"],
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
 };
