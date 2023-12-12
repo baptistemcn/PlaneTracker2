6dbc7bf4739f368b1b295beca5714727f3846334
Baptiste Marcon <baptistemcn@icloud.com>
2023-12-12 17:14:32 +0100

    feat(theming): finish screen

---
+++ src/authentication/Theming/Theming.spec.tsx
@@ -0,0 +1,111 @@
+import { render } from "@testing-library/react-native";
+
+import { ReThemeProvider } from "@theme";
+
+import { Themeing } from "./Theming";
+import { assertTextIsUppercase } from "@utils";
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({
+    t: (key: unknown) => key,
+    i18n: {
+      changeLanguage: jest.fn(),
+    },
+  }),
+}));
+
+describe("Theming screen", () => {
+  it("should render", () => {
+    render(
+      <ReThemeProvider>
+        <Themeing />
+      </ReThemeProvider>,
+    );
+  });
+
+  it("should render a title", () => {
+    const { getByTestId } = render(
+      <ReThemeProvider>
+        <Themeing />
+      </ReThemeProvider>,
+    );
+
+    const title = getByTestId("title");
+
+    expect(title).toBeTruthy();
+    expect(title).toBeOnTheScreen();
+
+    expect(title.props.children).toEqual("theming.message.title".toUpperCase());
+  });
+
+  it("should render a title in uppercase", async () => {
+    const { getByTestId } = render(
+      <ReThemeProvider>
+        <Themeing />
+      </ReThemeProvider>,
+    );
+
+    const title = getByTestId("title");
+
+    expect(title).toBeTruthy();
+    expect(title).toBeOnTheScreen();
+
+    await assertTextIsUppercase(getByTestId, "title");
+  });
+
+  it("should render a presentation text", () => {
+    const { getByTestId } = render(
+      <ReThemeProvider>
+        <Themeing />
+      </ReThemeProvider>,
+    );
+
+    const text = getByTestId("favorite");
+
+    expect(text).toBeTruthy();
+    expect(text).toBeOnTheScreen();
+
+    expect(text.props.children).toEqual("theming.message.favorite");
+  });
+
+  it("should render two theme button", () => {
+    const { getByTestId } = render(
+      <ReThemeProvider>
+        <Themeing />
+      </ReThemeProvider>,
+    );
+
+    const buttons = getByTestId("picker");
+
+    expect(buttons).toBeTruthy();
+    expect(buttons).toBeOnTheScreen();
+  });
+
+  it("should render a helping message", () => {
+    const { getByTestId } = render(
+      <ReThemeProvider>
+        <Themeing />
+      </ReThemeProvider>,
+    );
+
+    const text = getByTestId("help");
+
+    expect(text).toBeTruthy();
+    expect(text).toBeOnTheScreen();
+
+    expect(text.props.children).toEqual("theming.message.start");
+  });
+
+  it("should render a button to continue", () => {
+    const { getByTestId } = render(
+      <ReThemeProvider>
+        <Themeing />
+      </ReThemeProvider>,
+    );
+
+    const button = getByTestId("button");
+
+    expect(button).toBeTruthy();
+    expect(button).toBeOnTheScreen();
+  });
+});
--- src/authentication/Theming/Theming.tsx
+++ src/authentication/Theming/Theming.tsx
@@ -1,5 +1,52 @@
-import { Text } from "react-native";
+import { useTranslation } from "react-i18next";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+import { useTheme } from "@rneui/themed";
+
+import { Box, ReButton, ReText } from "@ui";
+
+import { ThemePicker } from "../components";
 
 export const Themeing = () => {
-  return <Text>Themeing</Text>;
+  const { theme } = useTheme();
+  const { t } = useTranslation();
+
+  const insets = useSafeAreaInsets();
+
+  return (
+    <Box
+      flex={1}
+      flexDirection={"column"}
+      paddingBotton={insets.bottom}
+      paddingTop={insets.top}
+      width={"100%"}
+    >
+      <Box flex={1} flexDirection={"column"} maxHeight={"15%"}>
+        <ReText
+          fontFamily="Bold"
+          fontSize={28}
+          letterSpacing={3}
+          testID="title"
+        >
+          {t("theming.message.title").toUpperCase()}
+        </ReText>
+      </Box>
+      <Box flex={1} alignItems={"center"} maxHeight={"30%"}>
+        <ReText testID="favorite">{t("theming.message.favorite")}</ReText>
+      </Box>
+      <Box flex={1} flexDirection={"column"} maxHeight={"15%"}>
+        <Box flexDirection={"row"} margin={theme.spacing.xs} testID="picker">
+          <ThemePicker />
+        </Box>
+        <Box margin={theme.spacing.xs}>
+          <ReText color={theme.colors.grey2} fontFamily="Light" testID="help">
+            {t("theming.message.start")}
+          </ReText>
+        </Box>
+      </Box>
+      <Box flex={1}>
+        <ReButton label={t("common.button.next").toUpperCase()} />
+      </Box>
+    </Box>
+  );
 };
