58568b0a3dc7f4520453b9a5bd7133953347b939
Baptiste Marcon <baptistemcn@icloud.com>
2023-12-16 18:02:28 +0100

    test(ui): add alert unit test

---
+++ src/ui/Alert/Alert.spec.tsx
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react-native";
+import { ReAlert } from "./Alert";
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({
+    t: (key: unknown) => key,
+    i18n: {
+      changeLanguage: jest.fn(),
+    },
+  }),
+}));
+
+describe("Alert component", () => {
+  it("should render", () => {
+    render(<ReAlert />);
+  });
+});
