3909576664ad889f951be99f3300c5ac4cf76094
Baptiste Marcon <baptistemcn@icloud.com>
2023-12-20 19:55:01 +0100

    refactor(ui): performance alert component

--- src/ui/Alert/Alert.tsx
+++ src/ui/Alert/Alert.tsx
@@ -1,23 +1,29 @@
-import { useEffect } from "react";
+import { memo, useCallback, useEffect } from "react";
 import { Alert } from "react-native";
 import { useTranslation } from "react-i18next";
 
 import { ReAlertProps } from "@types";
 
-export const ReAlert = ({
-  message = "Message",
-  title = "Title",
-}: ReAlertProps) => {
-  const { t } = useTranslation();
+export const ReAlert = memo(
+  ({ message = "Message", title = "Title" }: ReAlertProps) => {
+    const { t } = useTranslation();
 
-  useEffect(() => {
-    Alert.alert(t(title), t(message), [
-      {
-        text: t("alert.button.close"),
-        style: "cancel",
-      },
-    ]);
-  }, [message, t, title]);
+    const translatedTitle = t(title);
+    const translatedMessage = t(message);
 
-  return null;
-};
+    const showAlert = useCallback(() => {
+      Alert.alert(translatedTitle, translatedMessage, [
+        {
+          text: t("alert.button.close"),
+          style: "cancel",
+        },
+      ]);
+    }, [t, translatedTitle, translatedMessage]);
+
+    useEffect(() => {
+      showAlert();
+    }, [showAlert]);
+
+    return null;
+  },
+);
